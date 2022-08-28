ecc83ce63d2fb361d1fd9e3e039f0ef37ca8159b
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-08-28 22:26:20 +0200

    Advertisement details layout added

--- frontend/src/app/advertisement-details/advertisement-details.component.ts
+++ frontend/src/app/advertisement-details/advertisement-details.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Advertisement } from '../models';
 
 @Component({
   selector: 'app-advertisement-details',
   templateUrl: './advertisement-details.component.html',
   styleUrls: ['./advertisement-details.component.scss'],
 })
-export class AdvertisementDetailsComponent {}
+export class AdvertisementDetailsComponent implements OnInit {
+  advertisement: Advertisement;
+
+  constructor(private route: ActivatedRoute, private client: HttpClient) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      const id = params['id'];
+
+      this.client
+        .get<Advertisement>(`api/advertisements/${id}`)
+        .subscribe((advertisement) => (this.advertisement = advertisement));
+    });
+  }
+}
